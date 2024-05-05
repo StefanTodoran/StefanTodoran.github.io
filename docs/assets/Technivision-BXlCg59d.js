9cc65f98bb568359cd8f894a756eb3fc8539c92b
StefanTodoran <stefan.alex4@gmail.com>
2024-05-05 13:51:02 -0700

    built changes, published to production

---
+++ docs/assets/Technivision-BXlCg59d.js
@@ -0,0 +1,2 @@
+/*! For licenses information, see LICENSES.txt */
+import{j as i}from"./index-BhVzLL3k.js";const s="/assets/technivision-BnI09PRV.gif";function t(){return i.jsx("img",{className:"parallax-content",src:s})}export{t as default};
